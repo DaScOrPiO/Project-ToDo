3f1d033e7e6dff7f6d1cf1f658bfb3c5cc81bdee
DaScOrPiO <dunni82@yahoo.com>
2022-06-10 15:44:45 -0700

    refrenced

--- Project Todo/Todo.js
+++ Project Todo/Todo.js
@@ -1,12 +1,13 @@
+'use strict'
+
 function todo() {
     let app = prompt('WELCOME! Add an Item? Enter (n) To Begin, Enter (q) To Quit');
     const todo = ['*****']; // constant Todo on list!
     while (app !== 'q' && app !== 'quit') { // Quitting The Action
         if (app === 'n') { // Adding A new Todo to list
-            let newTodo = prompt('Enter A New Todo');
+            let newTodo = prompt('Enter A New Item');
             todo.push(newTodo);
-            console.log(`${newTodo} Added!`);
-            alert('Action Successful!😊 CHECK BROWSER CONSOLE!')
+            alert('Action Successful!😊')
         }
         else if (app === 'l') { // Listing Your Todo
             app = alert('Press The Ok Button To View Items In Console! ✔✔');
@@ -19,23 +20,21 @@ function todo() {
             let itemsNum = Number(prompt('Enter Amount of Items To Be Deleted NOTE: This Is From Point Index Selected'));
             if (!Number.isNaN(index) && !Number.isNaN(itemsNum)) {
                 let deleted = todo.splice(index, itemsNum);
-                console.log(`${deleted} removed!`);
-                deleted = alert('Item(s) Removed SucessFully! Check Console!😊');
-                console.log(`${todo}`);
-            } else { index = alert('Please Enter A Valid Number To Delete Item!🙄') }
+                alert(`${deleted} Binned! 🗑`);
+            } else { index = alert('Action Failed😢 Please Enter A valid Number') }
         }
         else if (app === 'arr') {
             let index1 = Number(prompt('Enter Index Where You Want Item Added'));
-            let amount = Number(prompt('Enter Amount Of Items To Be Added NOTE: This Is From Point Index Selected')); // Subject To be Worked on
+            let amount = Number(prompt('Enter Amount Of Items To Be Removed NOTE: This Is From Point Index Selected')); // Subject To be Worked on
             let item = prompt('Enter Item To Add');
-            if (!Number.isNaN(index1)) {
+            if (!Number.isNaN(index1) && !Number.isNaN(amount)) {
                 let ordered = todo.splice(index1, amount, item); // Subject To Be Worked On.
-                console.log(ordered);
-                console.log(todo);
+                alert(`${item} Added Sucessfully ✔`);
+                alert(`${ordered}Index Replaced`)
             } else { app = alert(`Someting's Wrong!😑 Please Follow Instructions Accordingly!`) }
         }
         app = prompt('What Would You Like Todo? Commands Include: (n(New Item), (d(Delete item)), (l(List Items)), (arr(Add & Place Item In a Specific Index)))');
     }
     console.log('You Quit The Operation!')
-    alert('Bye For Now😢')
+    alert('Operation Ended😢')
 }
